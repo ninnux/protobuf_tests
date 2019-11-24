d52fc45165416f0eea46c6164fea67d86beb5ba5
Nino <nino@ninux.org>
2019-11-25 00:22:43 +0100

    test lib

---
+++ c/myarr/test4.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "array.pb-c.h"
+
+
+
+int test4(){
+  Tutorial__Array array = TUTORIAL__ARRAY__INIT;
+  myarray_init(&array);
+
+
+  Tutorial__Myset *set0;
+  set0= malloc (sizeof (Tutorial__Myset)); //one insert at once
+
+  myarray_add_set(&array,set0,1574244029);
+  myarray_add_entry(&array,set0,"temp",19);
+  myarray_add_entry(&array,set0,"pres",1013);
+  myarray_add_entry(&array,set0,"batt",42);
+  myarray_add_entry(&array,set0,"wind",223);
+
+  //printf("key: %s\n", set0->entries[set0->n_entries-1]->key);
+  //printf("key: %s\n", array.sets[array.n_sets-1]->entries[set0->n_entries-1]->key);
+
+  Tutorial__Myset *set1;
+  set1= malloc (sizeof (Tutorial__Myset)); //one insert at once
+  myarray_add_set(&array,set1,1574244329);
+  myarray_add_entry(&array,set1,"batt",42);
+  myarray_add_entry(&array,set1,"wind",223);
+
+
+  Tutorial__Myset *set2;
+  set2= malloc (sizeof (Tutorial__Myset)); //one insert at once
+  myarray_add_set(&array,set2,1574244629);
+  myarray_add_entry(&array,set2,"temp",19);
+  myarray_add_entry(&array,set2,"pres",1013);
+
+
+  impacchetto(&array);
+  dealloco(&array);
+  return 0;
+}
+
+int main (int argc, const char * argv[]) 
+{
+ test4();
+return 0;
+}
